a5b6ba9e33a8966081ccc226d213b58a87560f84
Matias Sciancalepore <msciancalepore47@gmail.com>
2023-12-01 11:51:56 -0300

    terminado la parte de formulario

--- js/index.js
+++ js/index.js
@@ -15,10 +15,8 @@ document.addEventListener("DOMContentLoaded",()=>{
         if(!repuesto ==''){
             listarepuestos1.push(repuesto);
             ListaULRepuestos.innerHTML=``;
-            let i=0;
                 listarepuestos1.forEach(repuestoL => {
-                    ListaULRepuestos.innerHTML+=`<div class="contenedorRepuesto"><li class="liDeRepuestos">`+repuestoL+`</li><button class="btnBorrarRepuesto" id="`+i+`">Borrar</button></div>`;
-                    i++;
+                    ListaULRepuestos.innerHTML+=`<div class="contenedorRepuesto"><li class="liDeRepuestos">`+repuestoL+`</li><button class="btnBorrarRepuesto" id="`+listarepuestos1.indexOf(repuestoL)+`">Borrar</button></div>`;
                 });
                 asignarEventosABotones();
         }
@@ -28,17 +26,19 @@ document.addEventListener("DOMContentLoaded",()=>{
         botones.forEach(boton=>{
             boton.addEventListener('click',(e)=>{
                 e.preventDefault();
-                
+                console.log("lista valuada en boton id antes de borrarla= "+listarepuestos1[boton.id]);
                 // listarepuestos1=[];
                 delete listarepuestos1[boton.id];
+                console.log("boton id= "+boton.id);
+                console.log("lista valuada en boton id= "+listarepuestos1[boton.id]);
                 // listarepuestos1.splice(boton.id);
                 // console.log(listarepuestos1);
                 // console.log(boton.id);
-                let i=0;
+                // let i=0;
                 ListaULRepuestos.innerHTML='';
                 listarepuestos1.forEach(repuestoL => {
-                    ListaULRepuestos.innerHTML+=`<div class="contenedorRepuesto"><li class="liDeRepuestos">`+repuestoL+`</li><button class="btnBorrarRepuesto" id="`+i+`">Borrar</button></div>`;
-                    i++;
+                    ListaULRepuestos.innerHTML+=`<div class="contenedorRepuesto"><li class="liDeRepuestos">`+repuestoL+`</li><button class="btnBorrarRepuesto" id="`+listarepuestos1.indexOf(repuestoL)+`">Borrar</button></div>`;
+                    // i++;
                 });
                 asignarEventosABotones();
 
