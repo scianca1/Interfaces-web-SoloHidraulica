a7b6b8eb4690818b019be2ced94217d355a02744
Matias Sciancalepore <msciancalepore47@gmail.com>
2023-12-02 21:53:43 -0300

    arreglo un poco el estilo del pdf

--- js/index.js
+++ js/index.js
@@ -14,7 +14,8 @@ document.addEventListener("DOMContentLoaded",()=>{
  
 
     GenerarPDF.addEventListener("click",(e)=>{
-        e.preventDefault();
+        // e.preventDefault();
+        
             let eNombreCliente= document.getElementById('nombreCliente');
             let eApellidoCliente= document.getElementById('apellidoCliente');
             let ePatente= document.getElementById('patente');
@@ -35,75 +36,121 @@ document.addEventListener("DOMContentLoaded",()=>{
             let tituloReparacion=eTituloReparacion.value;
             let descripcion=eDescripcion.value;
             let nombreGenerador= eNombreGenerador.value;
+            if(patente!=""&&tituloReparacion!=""&&nombreGenerador!=""){
 
-            //valores de textos 
-            let opacidadTitulo = 0.7;
-            let opacidadDatos = 1;
-            let tamanioTitulos= 14;
-            let tamanioDatos= 21;
+                //valores de textos 
+                
+                let tamanioTitulos= 14;
+                let tamanioDatos= 21;
+
+                // Crear un nuevo objeto de fecha
+                var fechaActual = new Date();
+
+                // Obtener los componentes de la fecha
+                var año = fechaActual.getFullYear();
+                var mes = fechaActual.getMonth() + 1; // Los meses van de 0 a 11, así que sumamos 1
+                var dia = fechaActual.getDate();
+                var horas = fechaActual.getHours();
+                var minutos = fechaActual.getMinutes();
             
 
-            // Agrega contenido al PDF
-            doc.setFontSize(tamanioDatos);
-            doc.setOpacity(opacidadDatos);
-            doc.text(20, 20, 'Orden de trabajo');
-            doc.text(100, 20, 'Genero la orden:  '+nombreGenerador);
+                // Agrega contenido al PDF 
+                doc.setDrawColor(0, 50, 50);
+                doc.setLineWidth(15); // Grosor en unidades
+                doc.line(15, 18, 190, 18); // (x1, y1, x2, y2)
+                
+                doc.setDrawColor(70, 68, 68);
+                doc.line(15, 68, 190, 68); // (x1, y1, x2, y2)
 
-            doc.text(100, 35, patente);
-            
-            doc.setFontSize(tamanioTitulos);
-            doc.setOpacity(opacidadTitulo);
-            doc.text(40, 35, `Patente: `);
+                doc.line(15, 98, 190, 98); // (x1, y1, x2, y2)
 
-            doc.setFontSize(tamanioDatos);
-            doc.setOpacity(opacidadDatos);
-            doc.text(100, 45, modelo);
-            
-            doc.setFontSize(tamanioTitulos);
-            doc.setOpacity(opacidadTitulo);
-            doc.text(40, 45, `Modelo:`);
+                doc.line(15, 128, 190, 128); // (x1, y1, x2, y2)
 
-            doc.setFontSize(tamanioDatos);
-            doc.setOpacity(opacidadDatos);
-            doc.text(100, 55, anio);
+                doc.setFontSize(tamanioDatos);
+                
+                // doc.setOpacity(opacidadDatos);
+                doc.setTextColor(255, 255, 255);
+                doc.text(20, 20, 'ORDEN DE TRABAJO');
 
-            doc.setFontSize(tamanioTitulos);
-            doc.setOpacity(opacidadTitulo);
-            doc.text(40, 55, `AÑO:`);
+                doc.setFontSize(tamanioTitulos);
+                doc.text(110, 15, 'Genero la orden:  '+nombreGenerador);
+                doc.text(110, 22, 'Fecha:  '+dia+"/"+mes+"/"+año+"   "+horas+":"+minutos);
+                
+                doc.setTextColor(0, 0, 0);
+                doc.setFontSize(tamanioDatos);
+                doc.text(100, 35, patente);
 
-            doc.setFontSize(tamanioDatos);
-            doc.setOpacity(opacidadDatos);
-            doc.text(20, 70, `CLIENTE`);
+                
+                doc.setFontSize(tamanioTitulos);
+                doc.setTextColor(70, 68, 68);
+                // doc.setOpacity(opacidadTitulo);
+                doc.text(40, 35, `Patente: `);
 
-            doc.setFontSize(tamanioTitulos);
-            doc.setOpacity(opacidadTitulo);
-            doc.text(40, 85, `Nombre:`);
+                
+                doc.setFontSize(tamanioDatos);
+                doc.setTextColor(0, 0, 0);
+                // doc.setOpacity(opacidadDatos);
+                doc.text(100, 45, modelo);
+                
+                doc.setFontSize(tamanioTitulos);
+                doc.setTextColor(70, 68, 68);
+                // doc.setOpacity(opacidadTitulo);
+                doc.text(40, 45, `Modelo:`);
 
-            doc.setFontSize(tamanioDatos);
-            doc.setOpacity(opacidadDatos);
-            doc.text(100, 80, nombreCliente+" "+apellidoCliente);
+                doc.setFontSize(tamanioDatos);
+                doc.setTextColor(0, 0, 0);
+                // doc.setOpacity(opacidadDatos);
+                doc.text(100, 55, anio);
 
-            doc.text(20, 100, `DATOS DE REPARACION`);
+                doc.setFontSize(tamanioTitulos);
+                doc.setTextColor(70, 68, 68);
+                // doc.setOpacity(opacidadTitulo);
+                doc.text(40, 55, `AÑO:`);
 
-            doc.setFontSize(tamanioTitulos);
-            doc.setOpacity(opacidadTitulo);
-            doc.text(40, 115, 'REPARACION:');
+                doc.setFontSize(tamanioDatos);
+                doc.setTextColor(255, 255, 255);
+                // doc.setOpacity(opacidadDatos);
+                doc.text(85, 70, `CLIENTE`);
 
-            doc.setFontSize(tamanioDatos);
-            doc.setOpacity(opacidadDatos);
-            doc.text(100, 110, tituloReparacion);
-            doc.text(85, 130, 'Descripcion:');
-            // doc.text(40, 145, descripcion);
+                doc.setFontSize(tamanioTitulos);
+                doc.setTextColor(70, 68, 68);
+                // doc.setOpacity(opacidadTitulo);
+                doc.text(40, 85, `Nombre:`);
 
-            // Ajusta la posición y el ancho máximo para el texto largo
-            let descripcionLines = doc.splitTextToSize(descripcion, 150);
-            let descriptionY = 145;
+                doc.setFontSize(tamanioDatos);
+                doc.setTextColor(0, 0, 0);
+                // doc.setOpacity(opacidadDatos);
+                doc.text(100, 85, nombreCliente+" "+apellidoCliente);
 
-            // Agrega cada línea de la descripción
-            descripcionLines.forEach((line) => {
-                doc.text(20, descriptionY, line);
-                descriptionY += 10; // Ajusta el espaciado entre líneas según sea necesario
-            });
+                doc.setTextColor(255, 255, 255);
+                doc.text(60, 100, `DATOS DE REPARACION`);
+
+                doc.setFontSize(tamanioTitulos);
+                doc.setTextColor(70, 68, 68);
+                // doc.setOpacity(opacidadTitulo);
+                doc.text(40, 115, 'REPARACION:');
+
+                doc.setFontSize(tamanioDatos);
+                doc.setTextColor(0, 0, 0);
+                // doc.setOpacity(opacidadDatos);
+                doc.text(100, 115, tituloReparacion);
+
+                doc.setTextColor(255, 255, 255);
+                doc.text(85, 130, 'Descripcion:');
+                // doc.text(40, 145, descripcion);
+
+                // Ajusta la posición y el ancho máximo para el texto largo
+                doc.setTextColor(0, 0, 0);
+                let descripcionLines = doc.splitTextToSize(descripcion, 150);
+                let descriptionY = 145;
+
+                // Agrega cada línea de la descripción
+                descripcionLines.forEach((line) => {
+                    doc.text(20, descriptionY, line);
+                    descriptionY += 10; // Ajusta el espaciado entre líneas según sea necesario
+                });
+
+                
 
 
 
@@ -115,8 +162,9 @@ document.addEventListener("DOMContentLoaded",()=>{
 
 
 
-            // Guarda o descarga el PDF
-            doc.save('OrdenDeTrabajo.pdf');
+                // Guarda o descarga el PDF
+                doc.save(patente+'_O.pdf');
+        }
     });
     btnAgregarRepuesto.addEventListener('click',(e)=>{
         e.preventDefault();
